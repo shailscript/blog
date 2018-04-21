0360ac68b8f1ee78897b490207ac7232262306cc
Shailendra Shukla <shailendrashuklapro@gmail.com>
2018-04-21 09:56:11 +0530

    TutorPad description added

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,13 @@
       <div class="row">
         <div class="col-md-12">
           <h1>About us</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p> TutorPad is a one stop solution for Tuition classes. TutorPad is awesome. Add some nice description.
+              TutorPad is awesome. Add some nice description. TutorPad is awesome. Add some nice description.
+              TutorPad is a one stop solution for Tuition classes. TutorPad is awesome. Add some nice description.
+              TutorPad is awesome. Add some nice description. TutorPad is awesome. Add some nice description.
+              TutorPad is a one stop solution for Tuition classes. TutorPad is awesome. Add some nice description.
+              TutorPad is awesome. Add some nice description. TutorPad is awesome. Add some nice description.
+          </p>
         </div>
       </div>
   @endsection
