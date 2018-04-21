0ba9fda20f0f0df424651563cee747781ae92f11
Shailendra Shukla <shailendrashuklapro@gmail.com>
2018-04-21 09:53:40 +0530

    UI enhancememts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,21 +2,29 @@
   @section('title', '| Edit Post')
   @section('content')
     <div class="row">
-      <div class="col-sm-12">
+      <div class="col">
         <h1>Edit post</h1>
       </div>
-      <form action="{{ route('posts.update', ['id' => $post->id]) }}" class="form" method="post">
-        <div class="col-md-8">
+    </div>
+    <div class="row">
+      <div class="col-8">
+        <form action="{{ route('posts.update', ['id' => $post->id]) }}" class="form" method="post">
            {{ method_field('PUT') }}
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <label  for="title">Title:</label>
-          <input class="from-control input-lg" type="text" name="title" value="{{ $post->title }}">
-          <label  for="title">Slug:</label>
-          <input class="from-control" type="text" name="slug" value="{{ $post->slug }}">
-          <label for="body">Post body</label>
-          <input class="form-control" type="textarea" name="body" value=" {{ $post->body }}">
+          <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" class="form-control" name="title" value="{{ $post->title }}">
+          </div>
+          <div class="form-group">
+            <label for="slug">Slug:</label>
+            <input type="text" class="form-control" name="slug" value="{{ $post->slug }}">
+          </div>
+          <div class="form-group">
+            <label for="body">Post body</label>
+            <textarea class="form-control" rows="8" name="body" value="">{{ $post->body }}</textarea>
+          </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-4">
           <div class="well">
             <dl class="dl-horizontal">
               <dt>Url :</dt>
@@ -27,15 +35,15 @@
               <dd>{{ date('M j, Y h:i A', strtotime($post->updated_at)) }}</dd>
             </dl>
             <div class="row">
-              <div class="col-sm-6">
+              <div class="col">
                 <a href="{{ route('posts.show', $post->id)}}" class="btn btn-block btn-danger">Cancel</a>
               </div>
-              <div class="col-sm-6">
+              <div class="col">
                 <button type="submit" class="btn btn-success btn-block">Save changes</button>
               </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   @endsection
