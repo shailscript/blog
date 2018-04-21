d68c5f2d11e78a580bcffc1ad50875b565df51d0
Shailendra Shukla <shailendrashuklapro@gmail.com>
2018-04-21 09:54:06 +0530

    UI for blog CRUD changed using bs4

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,26 @@
   @section('title', '| Create New Post ')
     @section('content')
       <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col">
           <h1>Create Blog Post</h1>
-          <hr></hr>
+          <hr>
           <form class="form" action="{{  route('posts.store') }}" method="post">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <label  for="title">Title:</label>
-            <input type="text" name="title" value="">
-            <label  for="slug">Slug:</label>
-            <input type="text" name="slug" value="">
-            <label for="body">Post body</label>
-            <input type="textarea" name="body" value="">
-            <button class="btn btn-success">Create Post</button>
+            <div class="form-group">
+              <label for="title">Title:</label>
+              <input type="text" class="form-control" name="title" placeholder="Example: An Awesome blog coming up">
+            </div>
+            <div class="form-group">
+              <label for="slug">Slug:</label>
+              <input type="text" class="form-control" name="slug" placeholder="Example: awesome-blog">
+            </div>
+            <div class="form-group">
+              <label for="body">Post body</label>
+              <textarea class="form-control" rows="8" name="body" value="" placeholder="Some awesome text..."></textarea>
+            </div>
+            <br>
+            <button class="btn btn-success btn-lg btn-block">Create Post</button>
+            <br>
           </form>
         </div>
       </div>
