1944d3a5900e8e0890b7527120e0715673c88859
Shailendra Shukla <shailendrashuklapro@gmail.com>
2018-04-21 09:53:04 +0530

    UI according to bs4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,12 +2,14 @@
   @section('title','| All Posts')
   @section('content')
     <div class="row">
-      <div class="col-md-10">
+      <div class="col-9">
         <h1>All Posts</h1>
       </div>
-      <div class="col-md-2">
-        <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary">Create new post</a>
+      <div class="col-3">
+        <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-dark">Create new post</a>
       </div>
+    </div>
+    <div class="row">
       <div class="col-md-12">
         <hr>
       </div>
@@ -29,8 +31,8 @@
                 <td>{{ $post->title }}</td>
                 <td>{{ substr($post->body, 0, 50) }} {{ strlen($post->body)>50 ? "..." : ""}}</td>
                 <td>{{ date('M j Y, h:i A', strtotime( $post->created_at ) ) }}</td>
-                <td><a href=" {{ route('posts.show', ['id' => $post->id ]) }} " class = "btn btn-primary btn-block">View</a>
-                    <a href=" {{ route('posts.edit', ['id' => $post->id ]) }} " class = "btn btn-primary btn-block">Edit</a>
+                <td><a href=" {{ route('posts.show', ['id' => $post->id ]) }} " class = "btn btn-info btn-block">View</a>
+                    <a href=" {{ route('posts.edit', ['id' => $post->id ]) }} " class = "btn btn-warning btn-block">Edit</a>
                 </td>
               </tr>
             @endforeach
